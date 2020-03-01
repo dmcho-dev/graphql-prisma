0a93158eb040fcd7e8b997e1c691c18490ce222e
Dmcho <dmcho.dev@gmail.com>
2020-02-29 22:03:18 -0800

    done - 66. Closing Prisma to the Outside World

--- src/prisma.js
+++ src/prisma.js
@@ -1,8 +1,13 @@
 import { Prisma } from 'prisma-binding'
 
+/**
+ * 66. Closing Prisma to the Outside World
+ */
+
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'http://localhost:4466'
+    endpoint: 'http://localhost:4466',
+    secret: 'thisismysupersecrettext'
 })
 
  export { prisma as default }
